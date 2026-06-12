c7a87e818abd1beda3abc8923e134325e6a59e9f
João Gabriel <cirinojoao261@gmail.com>
2026-06-12 10:04:32 -0300

    feat: :sparkles: Codigo pokedex
    Novo codigo

---
+++ my-react-router-app/app/routes/type.tsx
@@ -0,0 +1,13 @@
+import { Tipos } from "~/tipos/Tipos";
+import type { Route } from "./+types/home";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Type() {
+  return <Tipos />;
+}
